47a0168a251bc5195b58d6dd9f4b58d57b9d40d5
taeyong <baeyong1227@gmail.com>
2022-11-08 23:49:39 -0500

    playoff simulation

--- main.py
+++ main.py
@@ -49,9 +49,11 @@ def start():
         "Would like you to simulate the whole playoffs or one series? Enter 1 for playoffs, 2 for one series: "))
     if user_choice == 2:
         series = Series()
-        series.simulate()
+        series.simulateSeries()
     elif user_choice == 1:
-        pass
+        playoffs = Playoffs()
+        playoffs.generateTeams()
+        playoffs.simulatePlayoffs()
 
 
 class Team:
@@ -60,6 +62,7 @@ class Team:
         self.conference = None
         self.rating = None
         self.team_name = None
+        self.seed = None
 
     def getAttributes(self):
         for i in sheet.iter_rows():
@@ -81,16 +84,20 @@ class Team:
 class Series():
     def __init__(self):
         self.winner = None
+        self.winner_id = None
         self.loser = None
+        self.loser_id = None
         self.games = 0
+        self.round_name = None
+        self.conference = None
 
-    def simulate(self):
-
-        team1_id = int(input("Enter in the id for the first team: "))
+    def simulateSeries(self, team1_id=None, team2_id=None):
+        if team1_id is None:
+            team1_id = int(input("Enter in the id for the first team: "))
         team1 = Team(team1_id)
         team1.getAttributes()
-
-        team2_id = int(input("Enter in the id for the second team: "))
+        if team2_id is None:
+            team2_id = int(input("Enter in the id for the second team: "))
         team2 = Team(team2_id)
         team2.getAttributes()
 
@@ -116,13 +123,113 @@ class Series():
 
         if team1_wins == 4:
             self.winner = team1
+            self.winner_id = team1.id
             self.loser = team2
+            self.loser_id = team2.id
+
         else:
             self.winner = team2
+            self.winner_id = team2.id
             self.loser = team1
+            self.loser_id = team1.id
 
-        print(
-            f"{self.winner} win the series against the {self.loser} in {self.games} games!")
+        # needs fixing
+        if self.conference is None:
+            print(
+                f"{self.winner} wins the series against the {self.loser} in {self.games} games!")
+        elif self.conference == "Finals":
+            print(
+                f"{self.winner} win the NBA FINALS against the {self.loser} in {self.games} games!")
+        else:
+            print(
+                f"{self.winner} win the {self.conference} {self.round_name} against the {self.loser} in {self.games} games!")
+            print()
+
+
+class Playoffs:
+    def __init__(self):
+        self.teams_east = []
+        self.teams_west = []
+
+    def generateTeams(self):
+        while len(self.teams_east) < 8:
+            random_east_id = random.randint(1, 30)
+            for i in sheet.iter_rows():
+                id = i[0].value
+                if id == random_east_id:
+                    conference = i[2].value
+                    if conference == "East" and id not in self.teams_east:
+                        self.teams_east.append(id)
+        while len(self.teams_west) < 8:
+            random_west_id = random.randint(1, 30)
+            for i in sheet.iter_rows():
+                id = i[0].value
+                if id == random_west_id:
+                    conference = i[2].value
+                    if conference == "West" and id not in self.teams_west:
+                        self.teams_west.append(id)
+
+    def simulatePlayoffs(self):
+        # East
+        current_round_east = self.teams_east
+        current_round_winners_east = []
+
+        rounds_finished_east = 0
+        while rounds_finished_east < 3:
+            i = 0
+            j = len(current_round_east) - 1
+            while i < j:
+                series = Series()
+                series.conference = "Eastern"
+                if rounds_finished_east == 0:
+                    series.round_name = "Conference Round One"
+                elif rounds_finished_east == 1:
+                    series.round_name = "Conference Semi Finals"
+                elif rounds_finished_east == 2:
+                    series.round_name = "Conference Finals"
+                series.simulateSeries(
+                    current_round_east[i], current_round_east[j])
+                current_round_winners_east.append(series.winner_id)
+                i += 1
+                j -= 1
+            rounds_finished_east += 1
+            current_round_east = current_round_winners_east
+            current_round_winners_east = []
+
+        # West
+        current_round_west = self.teams_west
+        current_round_winners_west = []
+
+        rounds_finished_west = 0
+        while rounds_finished_west < 3:
+            i = 0
+            j = len(current_round_west) - 1
+            while i < j:
+                series = Series()
+                series.conference = "Western"
+                if rounds_finished_west == 0:
+                    series.round_name = "Conference Round One"
+                elif rounds_finished_west == 1:
+                    series.round_name = "Conference Semi Finals"
+                elif rounds_finished_west == 2:
+                    series.round_name = "Conference Finals"
+                series.simulateSeries(
+                    current_round_west[i], current_round_west[j])
+                current_round_winners_west.append(series.winner_id)
+                i += 1
+                j -= 1
+            rounds_finished_west += 1
+            current_round_west = current_round_winners_west
+            current_round_winners_west = []
+
+        # Finals
+        finals = Series()
+        finals.conference = "Finals"
+        finals.simulateSeries(current_round_east[0], current_round_west[0])
+
+    def show(self):
+        print(self.teams_east)
+        print(self.teams_west)
 
 
 start = start()
